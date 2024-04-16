18868346e9bc0eae2ac1828802113ca1f51951d4
ahmedyousry74 <ayousry750@gmail.com>
2024-04-17 00:13:35 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'primary-color': '#324891',
+          'second-color': '#EAB221',
+          'third-color': '#231F20',
+          'fourth-color': '#636363',
+          'fifth-color': '#d3d3d3',
+          'white': '#fff',
+        },
+      },
+    },
+    plugins: [],
+  }
